f4adf360ddd87b73b02cf069cd6cb57b62c39b40
UmuTech <umustation7@outlook.com>
2017-03-31 20:14:35 +0100

    1

---
+++ Physics/Past_Papers/Units.php
@@ -0,0 +1,13 @@
+<?php
+echo "Here are our files";
+$path = ".";
+$dh = opendir(C:\Users\Umut Ekinci\Documents\GitHub\ut_downloads\Physics\Past_Papers\PA05);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='$path/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
